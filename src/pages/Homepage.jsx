a0657c8ba828f0a804659fea1e39acc43722065b
wdotgonzales <wdotgonzales@gmail.com>
2024-09-24 11:48:41 +0800

    adjust to max width

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -10,7 +10,7 @@ const Homepage = () => {
         <main className="max-w-[90%] sm:max-w-[80%] m-auto mt-3">
             <div className="bg-[#3A60E6] rounded">
                 <div className="flex flex-col lg:flex-row justify-between items-center px-[3em] sm:px-[5.5em] gap-5 lg:gap-0">
-                    <div className="mt-[2em] lg:mt-0 w-full lg:w-[42%]">
+                    <div className="mt-[2em] lg:mt-0 w-full max-w-[600px]">
                         <p className="text-3xl md:text-4xl lg:text-5xl text-white font-semibold mb-5">Book Appointment With Trusted Doctors</p>
                         <div className="flex flex-col lg:flex-row gap-3 mb-5">
                             <img src={groupProfiles} alt="" className="w-28 m-auto lg:m-0" />
