c404038cdc042f0006d892903cb8bac5ae1d9442
wdotgonzales <wdotgonzales@gmail.com>
2024-09-26 14:31:12 +0800

    last component for homepage done

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,6 +8,8 @@ import 'boxicons';
 import TopDoctors from "../components/TopDoctors";
 import appointment_img from "../assets/images/appointment_img-DzbZlMsi.png";
 
+import medibook_img_1 from '../assets/images/medibook-img-2.png';
+
 
 const Homepage = () => {
     return <>
@@ -44,7 +46,7 @@ const Homepage = () => {
             <section className="pb-[4em]">
                 <div className="bg-[#3A60E6] rounded h-auto md:h-[400px] py-[6em] md:py-0">
                     <div className="md:flex md:items-center md:justify-between mx-[4em]">
-                        <div className="md:max-w-[37%]">
+                        <div className="sm:max-w-[90%] md:max-w-[37%]">
                             <p className="text-2xl md:text-3xl lg:text-4xl text-white font-semibold mb-4" style={{ lineHeight: "1.2em" }}>Book Appointment With 100+ Trusted Doctors</p>
                             <Link to="/">
                                 <Button
@@ -68,6 +70,42 @@ const Homepage = () => {
                     </div>
                 </div>
             </section>
+
+            <article className="pt-[1.3em] pb-[5em]">
+                <div className="flex flex-col gap-0 md:flex-row md:gap-24">
+
+                    <div className="w-auto md:w-[50%]">
+                        <div className="flex items-center">
+                            <img src={medibook_img_1} alt="MediBook Logo" className="w-[80px]" />
+                            <p className="text-[1.7em]">
+                                <span className="font-bold text-[#3A60E6]">Medi</span>
+                                <span className="font-bold text-[#67C8FF]">Book</span>
+                            </p>
+                        </div>
+                        <p className="text-sm ml-3 mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    </div>
+
+                    <div className="ml-3 md:ml-0">
+                        <p className="font-bold text-2xl mt-8 mb-6">COMPANY</p>
+                        <ul className="flex flex-col gap-3">
+                            <li><p className="text-sm">Home</p></li>
+                            <li><p className="text-sm">About Us</p></li>
+                            <li><p className="text-sm">Delivery</p></li>
+                            <li><p className="text-sm">Privacy Policy</p></li>
+                        </ul>
+                    </div>
+
+                    <div className="ml-3 md:ml-0">
+                        <p className="font-bold text-2xl mt-8 mb-6">GET IN TOUCH</p>
+                        <ul className="flex flex-col gap-3">
+                            <li><p className="text-sm">+0-000-000-000</p></li>
+                            <li><p className="text-sm">wdotgonzales@gmail.com</p></li>
+                        </ul>
+                    </div>
+
+                </div>
+            </article>
+
         </main>
     </>
 }
