8d863fa08184ba42d81556aac71594457102d34f
wdotgonzales <wdotgonzales@gmail.com>
2024-09-27 08:51:42 +0800

    make medi book information to an independent component

---
+++ src/components/MediBookInformation.jsx
@@ -0,0 +1,40 @@
+import medibook_img_2 from '../assets/images/medibook-img-2.png';
+
+const MediBookInformation = () => {
+    return <article className="md:pt-[1.3em] pb-[7em]">
+        <div className="flex flex-col gap-0 md:flex-row md:gap-24">
+
+            <div className="w-auto md:w-[50%]">
+                <div className="flex items-center">
+                    <img src={medibook_img_2} alt="MediBook Logo" className="w-[80px]" />
+                    <p className="text-[1.7em]">
+                        <span className="font-bold text-[#3A60E6]">Medi</span>
+                        <span className="font-bold text-[#67C8FF]">Book</span>
+                    </p>
+                </div>
+                <p className="text-sm ml-3 mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+
+            <div className="ml-3 md:ml-0">
+                <p className="font-bold text-2xl mt-8 mb-6">COMPANY</p>
+                <ul className="flex flex-col gap-3">
+                    <li><p className="text-sm">Home</p></li>
+                    <li><p className="text-sm">About Us</p></li>
+                    <li><p className="text-sm">Delivery</p></li>
+                    <li><p className="text-sm">Privacy Policy</p></li>
+                </ul>
+            </div>
+
+            <div className="ml-3 md:ml-0">
+                <p className="font-bold text-2xl mt-8 mb-6">GET IN TOUCH</p>
+                <ul className="flex flex-col gap-3">
+                    <li><p className="text-sm">+0-000-000-000</p></li>
+                    <li><p className="text-sm">wdotgonzales@gmail.com</p></li>
+                </ul>
+            </div>
+
+        </div>
+    </article>
+}
+
+export default MediBookInformation;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -6,10 +6,8 @@ import SpecialitySlick from "../components/SpecialitySlick";
 
 import 'boxicons';
 import TopDoctors from "../components/TopDoctors";
+import MediBookInformation from "../components/MediBookInformation";
 import appointment_img from "../assets/images/appointment_img-DzbZlMsi.png";
-
-import medibook_img_1 from '../assets/images/medibook-img-2.png';
-
 
 const Homepage = () => {
     return <>
@@ -71,40 +69,7 @@ const Homepage = () => {
                 </div>
             </section>
 
-            <article className="pt-[1.3em] pb-[7em]">
-                <div className="flex flex-col gap-0 md:flex-row md:gap-24">
-
-                    <div className="w-auto md:w-[50%]">
-                        <div className="flex items-center">
-                            <img src={medibook_img_1} alt="MediBook Logo" className="w-[80px]" />
-                            <p className="text-[1.7em]">
-                                <span className="font-bold text-[#3A60E6]">Medi</span>
-                                <span className="font-bold text-[#67C8FF]">Book</span>
-                            </p>
-                        </div>
-                        <p className="text-sm ml-3 mt-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                    </div>
-
-                    <div className="ml-3 md:ml-0">
-                        <p className="font-bold text-2xl mt-8 mb-6">COMPANY</p>
-                        <ul className="flex flex-col gap-3">
-                            <li><p className="text-sm">Home</p></li>
-                            <li><p className="text-sm">About Us</p></li>
-                            <li><p className="text-sm">Delivery</p></li>
-                            <li><p className="text-sm">Privacy Policy</p></li>
-                        </ul>
-                    </div>
-
-                    <div className="ml-3 md:ml-0">
-                        <p className="font-bold text-2xl mt-8 mb-6">GET IN TOUCH</p>
-                        <ul className="flex flex-col gap-3">
-                            <li><p className="text-sm">+0-000-000-000</p></li>
-                            <li><p className="text-sm">wdotgonzales@gmail.com</p></li>
-                        </ul>
-                    </div>
-
-                </div>
-            </article>
+            <MediBookInformation />
 
         </main>
     </>
